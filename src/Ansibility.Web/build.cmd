5010611fd16f942cbf110f97106cb0ea8c267de1
Newbe <472158246@qq.com>
2017-01-15 13:27:14 +0800

    fix name bug

--- src/Ansibility.Web/build.cmd
+++ src/Ansibility.Web/build.cmd
@@ -1,7 +1,7 @@
-docker stop Ansibility
-docker rm Ansibility
-docker rmi Ansibility 
+docker stop ansibility
+docker rm ansibility
+docker rmi ansibility 
 del /f /q/ s out
 dotnet publish -c Debug -o out 
-docker build -t Ansibility .
-docker run -d -p 8000:80 --name Ansibility Ansibility
+docker build -t ansibility .
+docker run -d -p 8000:80 --name ansibility ansibility
