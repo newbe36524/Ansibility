86bb5cb08612ba68dfe55d0b25850a24e9b2ffad
Newbe <472158246@qq.com>
2017-01-31 20:09:45 +0800

    add easyui

--- src/Ansibility.Web/Controllers/HomeController.cs
+++ src/Ansibility.Web/Controllers/HomeController.cs
@@ -12,46 +12,6 @@ namespace Ansibility.Web.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult TestConsole(string prc, string q)
-        {
-            var process = new Process
-            {
-                StartInfo = new ProcessStartInfo(prc)
-                {
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    Arguments = q
-                }
-            };
-            process.Start();
-            var readToEnd = process.StandardOutput.ReadToEnd();
-            var allLines = !string.IsNullOrEmpty(readToEnd) ? readToEnd : process.StandardError.ReadToEnd();
-            ViewBag.Raw = allLines;
-            process.WaitForExit();
-            var re = new List<string>();
-            foreach (var line in allLines.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries))
-            {
-                re.Add(line);
-            }
-            ViewBag.Output = re;
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View();
---
+++ src/Ansibility.Web/wwwroot/lib/easyui/README.md
@@ -0,0 +1,2 @@
+# easyui
+> easyui前端框架 [http://www.jeasyui.com/](http://www.jeasyui.com/ "EasyUI")
