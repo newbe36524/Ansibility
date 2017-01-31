afa77a9f3870f38e4927347aa500c0c1432e0b85
Newbe <472158246@qq.com>
2017-01-31 22:09:21 +0800

    upgrade xunit

---
+++ src/Ansibility.Web.Tests/Services/TaskResultStoreTests.cs
@@ -0,0 +1,20 @@
+﻿using Ansibility.Web.Services;
+using Ansibility.Web.Services.Impl;
+using Autofac.Extras.Moq;
+using FluentAssertions;
+
+namespace Ansibility.Web.Tests.Services
+{
+    public class TaskResultStoreTests
+    {
+        public async void GetNullResult()
+        {
+            using (var mock = AutoMock.GetStrict())
+            {
+                ITaskResultStore service = mock.Create<TaskResultStore>();
+                var result = await service.GetResultAsync("notExists");
+                result.Should().BeNull();
+            }
+        }
+    }
+}
